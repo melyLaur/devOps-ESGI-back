449089dc23a535597381023942bbfcf15a4c505b
melis <melissanoemi.laurent@free.fr>
2023-12-09 19:49:45 +0100

    Add unit test

--- article/tests.py
+++ article/tests.py
@@ -1,3 +1,14 @@
-from django.test import TestCase
+#from django.test import TestCase
+import unittest
 
 # Create your tests here.
+class YourTestCase(unittest.TestCase):
+    def test_is_true(self):
+        self.assertTrue(True, "Test => OK")
+
+    def test_is_false(self):
+        self.assertFalse(False, "Test => OK")
+
+
+    #def test_should_not_pass_false_is_not_equal_true(self):
+       #self.assertTrue(False, "Test => Not OK")        
